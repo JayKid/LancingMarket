e067c8306bff0684048e767cc8021d421d9c4bef
Jay Kid <cyph3rfox@gmail.com>
2012-04-19 13:42:45 +0200

    Rails App skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LancingMarket::Application.config.secret_token = '669cb7710b3ec480bc50a573ee6d8c8481999d381e01ff0a357667a39044dc28aa21e636b15d77709a6d20d971a7368104036e9f9212190f61c02f49234fedd2'
